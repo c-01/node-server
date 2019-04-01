2f3750a0e4414452eed1354412ab0877d475d8c5
hideMan <thanhdntage@gmail.com>
2019-04-01 22:30:53 +0700

    path obj

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 
-const lg = require('./logger');
+const lon = require('./logger');
+const path = require('path');
 
+var pathObj = path.parse(__dirname);
 
-
-
+console.log(pathObj);
 
 
--- logger.js
+++ logger.js
@@ -1,19 +1,2 @@
-
-
-//#(function(exports, module, __filename, __dirname){
-
-console.log(__filename);
-console.log(__dirname);
-
-
-var url = 'http://get.io/log';
-
-function logs(msg1) {
-    console.log(msg1);
-}
-
-//# module.exports = logs;
-module.exports.got = logs;
-//# exports = logs; // module.exports
-
-//#})();
+const path = require('path');
+console.log(path.parse(__filename));
