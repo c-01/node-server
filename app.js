882aed8270068511b60b10c3c78782464bbe93f1
hideMan <thanhdntage@gmail.com>
2019-04-01 21:25:34 +0700

    ex for global

---
+++ app.js
@@ -0,0 +1,19 @@
+function running(type) {
+    console.log(type + ' very fast');
+    
+}
+//running('strong');
+// console.log(window); // window is not defined
+// console.log(); // global
+
+// setTimeout();
+// clearTimeout();
+
+// setInterval();
+// clearInterval();
+
+var msg = ''; // only in app.js
+console.log(global.msg); // undefined
+
+
+
