4afb46e93572e23801f5e2c30471db032346aac6
hideMan <thanhdntage@gmail.com>
2019-04-01 22:18:24 +0700

    filename

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 
 const lg = require('./logger');
 
-console.log(lg.baseurl); //http://get.io/log
-lg.got('this oke'); //  call function logs() this oke
 
-
-//console.log(module);
 
 
 
--- logger.js
+++ logger.js
@@ -1,9 +1,19 @@
 
+
+//#(function(exports, module, __filename, __dirname){
+
+console.log(__filename);
+console.log(__dirname);
+
+
 var url = 'http://get.io/log';
 
 function logs(msg1) {
     console.log(msg1);
 }
 
+//# module.exports = logs;
 module.exports.got = logs;
-module.exports.baseurl = url;
+//# exports = logs; // module.exports
+
+//#})();
